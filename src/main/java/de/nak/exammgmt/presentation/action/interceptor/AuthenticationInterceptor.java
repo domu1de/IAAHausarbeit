ee2f61042ccca1cebbe2365d4d174990474e6fe3
Domenic Muskulus <domenic@muskulus.eu>
2014-12-19 19:38:46 +0100

    Do no authenticate on action chaining

--- src/main/java/de/nak/exammgmt/presentation/action/interceptor/AuthenticationInterceptor.java
+++ src/main/java/de/nak/exammgmt/presentation/action/interceptor/AuthenticationInterceptor.java
@@ -6,7 +6,7 @@
 package de.nak.exammgmt.presentation.action.interceptor;
 
 import com.opensymphony.xwork2.ActionInvocation;
-import com.opensymphony.xwork2.interceptor.Interceptor;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 import de.nak.exammgmt.persistence.entity.user.User;
 import de.nak.exammgmt.presentation.action.BaseAction;
 import de.nak.exammgmt.service.authentication.AuthenticationService;
@@ -17,27 +17,31 @@ import de.nak.exammgmt.service.authentication.AuthenticationService;
  *
  * @author Alexander Mersmann <alexander.mersmann@nordakademie.de>
  */
-public class AuthenticationInterceptor implements Interceptor {
+public class AuthenticationInterceptor extends AbstractInterceptor {
 
     private AuthenticationService authenticationService;
 
     @Override
-    public void destroy() {
-    }
-
-    @Override
-    public void init() {
-    }
-
-    @Override
     public String intercept(ActionInvocation actionInvocation) throws Exception {
-        User user = authenticationService.authenticate();
+        if (shouldAuthenticate(actionInvocation)) {
+            User user = authenticationService.authenticate();
 
-        if (actionInvocation.getAction() instanceof BaseAction) {
-            ((BaseAction) actionInvocation.getAction()).setCurrentUser(user);
+            if (actionInvocation.getAction() instanceof BaseAction) {
+                ((BaseAction) actionInvocation.getAction()).setCurrentUser(user);
+            }
         }
 
         return actionInvocation.invoke();
+    }
+
+    /**
+     * Checks if authentication should happen. This is true if the invocation is not chained.
+     *
+     * @param actionInvocation the action invocation to check
+     * @return if authentication should happen
+     */
+    private boolean shouldAuthenticate(ActionInvocation actionInvocation) {
+        return actionInvocation.getStack().getRoot().size() < 3;
     }
 
     public void setAuthenticationService(AuthenticationService authenticationService) {
